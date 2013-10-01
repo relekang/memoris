daa47a6f65e675454dd3a039dbff75a611c74b66
Rolf Erik Lekang <me@rolflekang.com>
2013-10-01 13:44:13 +0200

    fix handling of post requests in api

--- memoris.py
+++ memoris.py
@@ -13,11 +13,8 @@ def index():
 
 
 @app.route('/<key>')
-def api(key):
+def get_api(key):
     r = redis.Redis()
-    if request.method == 'POST':
-        r.set(key, request.args.get('value'))
-
     value = r.get(key)
     if not value:
         return json_response({'error': 'No value for given key'}, 404)
@@ -25,5 +22,19 @@ def api(key):
     return json_response({
         key: value
     })
+
+
+@app.route('/<key>', methods=['POST'])
+def post_api(key):
+    r = redis.Redis()
+    if request.method == 'POST':
+        r.set(key, request.values.get('value'))
+
+    value = r.get(key)
+
+    return json_response({
+        key: value
+    })
+
 if __name__ == '__main__':
     app.run()
