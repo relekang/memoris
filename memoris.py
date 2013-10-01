82edb5081b21537d92ee683e301b0b2a49c83a61
Rolf Erik Lekang <me@rolflekang.com>
2013-10-01 14:33:26 +0200

    add support for local settings

--- memoris.py
+++ memoris.py
@@ -6,6 +6,14 @@ from utils import json_response
 
 app = Flask(__name__)
 
+try:
+    from local import local_config
+    app.config.update(local_config)
+except ImportError:
+    print "Could not import local config"
+
+r = redis.Redis(db=app.config['REDIS_DB'])
+
 
 @app.route('/')
 def index():
@@ -14,7 +22,6 @@ def index():
 
 @app.route('/<key>')
 def get_api(key):
-    r = redis.Redis()
     value = r.get(key)
     if not value:
         return json_response({'error': 'No value for given key'}, 404)
@@ -26,7 +33,6 @@ def get_api(key):
 
 @app.route('/<key>', methods=['POST'])
 def post_api(key):
-    r = redis.Redis()
     if request.method == 'POST':
         r.set(key, request.values.get('value'))
 
